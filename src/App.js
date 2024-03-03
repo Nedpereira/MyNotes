37f0320d0103860f9b9f20d5366f67702b21b5af
Nedpereira <need_vips@hotmail.com>
2024-03-03 15:23:29 -0300

    ✨ feat: adicionado a estrutura routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import Header from "./Components/Header";
+import ChatIA from "./Pages/Chat-IA";
 import Home from "./Pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ChatIA" element={<ChatIA />} />
+        <Route path="/Documentacao" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
